f90906243c0cf2a123c4ba38b1619a1cc4431327
bilal yakut <bilalykt21@gmail.com>
2022-11-11 01:16:26 +0300

    first commit

---
+++ src/main/java/org/example/reverseNumber/Main.java
@@ -0,0 +1,14 @@
+package org.example.reverseNumber;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Enter Number:");
+        Scanner scan=new Scanner(System.in);
+        int num=scan.nextInt();
+        ReverseNumber rNumber=new ReverseNumber();
+        System.out.println("Reverse of number U enter-->" + rNumber.reverse(num));
+    }
+}
---
+++ src/main/java/org/example/reverseNumber/ReverseNumber.java
@@ -0,0 +1,23 @@
+package org.example.reverseNumber;
+
+public class ReverseNumber {
+
+   // Given a signed 32-bit integer x, return x with its digits reversed.
+        public int reverse(int x) {
+            boolean isNegative = x < 0;
+
+            x = Math.abs(x);
+
+            int num = 0;
+
+            while (x > 0) {
+                if (Integer.MAX_VALUE / 10 < num) return 0;
+
+                num = 10 * num + x % 10;
+                x /= 10;
+            }
+
+            return isNegative ? -num : num;
+        }
+
+}
