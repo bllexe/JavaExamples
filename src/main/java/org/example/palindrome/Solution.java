6099ab8575a840d3db824d1ffd516662d81ca158
bilal yakut <bilalykt21@gmail.com>
2022-11-11 13:35:08 +0300

    second commit

---
+++ src/main/java/org/example/palindrome/Solution.java
@@ -0,0 +1,19 @@
+package org.example.palindrome;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        String str="kabak",reverseStr="";
+
+        for(int i=(str.length()-1);i>0;--i){
+            reverseStr+=str.charAt(i);
+        }
+
+        if (str.toLowerCase().equals(reverseStr.toLowerCase())){
+            System.out.println(str + " is a polindrome");
+        }else
+            System.out.println(str + " is not a polindrome");
+
+    }
+}
---
+++ src/main/java/org/example/palindrome/main.java
@@ -0,0 +1,8 @@
+package org.example.palindrome;
+
+public class main {
+
+    public static void main(String[] args) {
+
+    }
+}
