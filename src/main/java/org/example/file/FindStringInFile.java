35787dc82e58fcb027f5031b45817142d2730a4e
bilal yakut <bilalykt21@gmail.com>
2022-12-25 20:45:21 +0300

    adding gitignore file

--- src/main/java/org/example/file/FindStringInFile.java
+++ src/main/java/org/example/file/FindStringInFile.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class FingStringInFile {
+public class FindStringInFile {
 
     public static void main(String[] args) throws FileNotFoundException {
 
